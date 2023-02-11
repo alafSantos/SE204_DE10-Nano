cdb63ee56803823c77e1cfa2df449e3b92579976
alafSantos <alaf.ns4@gmail.com>
2023-02-11 10:22:57 +0100

    pipelined_wb_bram

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -12,16 +12,19 @@ module wb_bram #(
     wshb_if.slave wb_s
 );
   // a vous de jouer a partir d'ici
-  logic ack_w, ack_r, ack_r2;
+  logic ack_w, ack_r, ack_r2, adr_counter = 0;
   logic [3:0][7:0] memory[0:2**mem_adr_width-1];
-  wire [mem_adr_width-1:0] adr_i = wb_s.adr[mem_adr_width+1:2];
+  wire [mem_adr_width-1:0] adr_i;
+
+  // Address handler
+  assign adr_i = wb_s.adr[mem_adr_width+1:2] + adr_counter;
 
   // 0 in our slaves
   assign wb_s.err = 0;
   assign wb_s.rty = 0;
 
   // ACK
-  assign ack_w = wb_s.we & wb_s.stb;
+  assign ack_w = wb_s.we && wb_s.stb;
   assign ack_r2 = !wb_s.we & wb_s.stb;
   assign wb_s.ack = ack_w | ack_r;
 
@@ -32,18 +35,17 @@ module wb_bram #(
     end
 
     // Reading
-    if (ack_r2) begin
-      // Classic bus cycle
-      if (!(wb_s.cti[0]||wb_s.cti[1])) begin //cti = '000'
-        if (!ack_r) begin
-          for (int i = 0; i < 4; i++) begin
-            if (wb_s.sel[i]) begin
-              wb_s.dat_sm[(8*i+7)-:8] <= memory[adr_i][i];
-            end
-          end
-        end
+    if(ack_r2) begin
+      if(wb_s.cti[2] == wb_s.cti[1]) begin // Classic cycle and End-of-Burst
+        adr_counter <= 0;
         ack_r <= !ack_r;
       end
+      else if (wb_s.cti[1]) begin // Incrementing burst cycle
+        adr_counter <= 1;
+      end else begin // Constant address burst cycle
+        adr_counter <= 0;
+      end
+      wb_s.dat_sm <= memory[adr_i];
     end
 
     // Writing
